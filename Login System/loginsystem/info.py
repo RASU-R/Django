b82a702b8661105e01b17b16cb82bb7465689b21
Rasu R <108165654+RASU-R@users.noreply.github.com>
2023-06-16 14:01:01 +0530

    2nd

--- Login System/loginsystem/info.py
+++ Login System/loginsystem/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='rasuking2003@gmail.com'
-EMAIL_HOST_PASSWORD="wccentgajdjjmyzh"  #change from ->security -app password 
+EMAIL_HOST_USER='yourid@gmail.com'
+EMAIL_HOST_PASSWORD="password"  #change from ->security -app password 
 EMAIL_PORT=587
